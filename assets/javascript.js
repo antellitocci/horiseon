92b0e9af52c4955be016f6ebf6570410d2c5dc51
raddymaster <antellitocci@gmail.com>
2021-04-18 14:39:40 -0400

    created javscript file to handle scroll to top arrow and further css comments

---
+++ assets/javascript.js
@@ -0,0 +1,28 @@
+
+topButton = document.getElementById("toTopButton");
+
+//When user scrolls down 76px from the top (Header size) of the document, show button
+window.onscroll = function() {scrollFunction()};
+
+//Scroll function
+function scrollFunction()
+{
+    //create a variable to retrieve the button
+    topButton = document.getElementById("toTopButton");
+
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500)
+    {
+        topButton.style.display = "inline-block";
+    }
+    else
+    {
+        topButton.style.display = "none";
+    }
+}
+
+//Scroll to top of document, when the user clicks the button
+function topFunction()
+{
+    document.body.scrollTop = 0; //Safari support
+    document.documentElement.scrollTop = 0; //Chrome, Firefox, IE, and Opera support
+}
